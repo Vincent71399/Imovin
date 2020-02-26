8b41673bc1c838736c80e410c99724a4e4055920
vincent <vincent71399@gmail.com>
2020-02-26 13:09:22 +0800

    -implement reward

--- app/src/main/java/sg/edu/nus/imovin/Fragments/RewardsFragment.java
+++ app/src/main/java/sg/edu/nus/imovin/Fragments/RewardsFragment.java
@@ -2,12 +2,10 @@ package sg.edu.nus.imovin.Fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
